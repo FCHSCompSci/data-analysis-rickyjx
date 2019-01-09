85a05e7312befde512a27fca5d19d86552bdadf8
BJRUSH01 <bainca.rushrox@stu.jefferson.kyschools.us>
2019-01-09 08:47:46 -0500

    commit.

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Nani?")
+import csv
+
+pop = []
+arrests = []
+filename = 'drug-arrests-in-the-us.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    for row in reader:
+        try:
+            pop.append(row[1])
+            arrests.append(row[5])
+        except IndexError:
+            print("Error")
+
+
+    print(pop)
+    print(arrests)
+
+    percentages = []
+
+    for pop in pop:
+        for arrest in arrests:
+            percentages.append(int(arrest) / int(pop))
+
+    print(percentages)
